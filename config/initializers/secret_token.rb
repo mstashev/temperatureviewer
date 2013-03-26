9777af51b08d293bbb971cb0366e9ea33d1d11b8
mstashev <mstashev@gmail.com>
2013-03-26 11:49:36 -0400

    First Commit with Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Temperatureviewer::Application.config.secret_token = 'd8f2c082541b954520adcd5c2ac548ad5aa666f8bfb23983c26b4645d08ea751b5d6743ba3ff9b9e1f94899a586d6c0145b59d36bb96fea60d801e07a666ee02'
